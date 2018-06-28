bdc5a80fb86eed1410e63900e87929dff3ec6b44
Mohamed Magdy <moh.magdy40@gmail.com>
2018-06-28 19:37:57 +0200

    [IMP] Add details the manifest file about the author, webisite and a short description.

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoo_numverify",
+    'name': "Odoo NumVerify",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    Verify phone numbers in Odoo checkout form using NumVerify APIs
+    """,
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "MorePython",
+    'website': "http://www.morepython.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
