628f97c1936b1242e6920bf41bdadb4bdab40586
Mohamed Magdy <moh.magdy40@gmail.com>
2018-06-28 19:43:43 +0200

    [IMP] Improve checkout_form_validate() to perform the verification process for phone numbers vs countries.

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,20 +1,46 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+import requests
+from werkzeug.exceptions import Forbidden
 
-# class OdooNumverify(http.Controller):
-#     @http.route('/odoo_numverify/odoo_numverify/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+from odoo import _
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
-#     @http.route('/odoo_numverify/odoo_numverify/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('odoo_numverify.listing', {
-#             'root': '/odoo_numverify/odoo_numverify',
-#             'objects': http.request.env['odoo_numverify.odoo_numverify'].search([]),
-#         })
 
-#     @http.route('/odoo_numverify/odoo_numverify/objects/<model("odoo_numverify.odoo_numverify"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('odoo_numverify.object', {
-#             'object': obj
-#         })
+class WebsiteSale(WebsiteSale):
+
+    def checkout_form_validate(self, mode, all_form_values, data):
+        """
+        This method is modifying the original one to check if the phone number that the customer will add in the
+        checkout form is valid and matching with the country
+        :param mode: tuple ('new/edit', 'billing/shipping')
+        :param all_form_values: dict Holds all values before preprocess
+        :param data: dict Holds values after preprocess
+        :return:
+        error: Specifying if a certain field is not valid.
+        error_message: The message that will be raised if a certain field is not valid.
+        """
+
+        error, error_message = super(WebsiteSale, self).checkout_form_validate(mode=mode, all_form_values=all_form_values, data=data)
+
+        config = request.env['res.config.settings'].sudo().create({})
+
+        # Get the country's code for the country that the user has entered
+        country_code = request.env['res.country'].sudo().search([('id', '=', all_form_values.get('country_id'))], limit=1).code
+
+        # Check if the VerifyNum is enabled
+        if config.enable_verifynum:
+            api_access_key = config.verifynum_api_key
+            url = 'http://apilayer.net/api/validate?access_key=%s&number=%s&format=1' % (
+            api_access_key, all_form_values.get('phone'))
+
+            response = requests.get(url).json()
+
+            # Add the errors that will be raised if something was not valid.
+            if 'valid' in response.keys() and response.get('valid') and not response.get('country_code') == country_code:
+                error['country_id'] = 'error'
+                error_message.append(_('The country is not matching the phone number'))
+            elif 'valid' in response.keys() and not response.get('valid'):
+                error['phone'] = 'error'
+                error_message.append(_('Phone Number is not correct'))
+        return error, error_message
