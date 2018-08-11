74c9e683a74ace2192cda02c78186884aa04f5a6
Mohamed Magdy <moh.magdy40@gmail.com>
2018-08-11 04:18:09 +0200

    [ADD] Add constrains to check valid phones and valid mobiles.

--- models/models.py
+++ models/models.py
@@ -1,15 +1,62 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+import requests
+from odoo import models, api, _
+from odoo.exceptions import UserError
 
-# class odoo_numverify(models.Model):
-#     _name = 'odoo_numverify.odoo_numverify'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class Partner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    @api.constrains('country_id', 'phone')
+    def _check_country_vs_phone(self):
+        for rec in self:
+            if rec.country_id and rec.phone:
+                # Get the country's code for the country that the user has entered
+                country_code = rec.country_id.code
+
+                # Check if the VerifyNum is enabled
+
+                params = rec.env['ir.config_parameter'].sudo()
+                enable_verifynum=params.get_param('enable_verifynum')
+                verifynum_api_key=params.get_param('verifynum_api_key', default='')
+
+                if enable_verifynum and verifynum_api_key:
+                    url = 'http://apilayer.net/api/validate?access_key=%s&number=%s&format=1' % (verifynum_api_key, rec.phone)
+                    response = requests.get(url).json()
+                    # Raise the errors if something was not valid.
+                    if 'valid' in response.keys() and response.get('valid') and not response.get('country_code') == country_code:
+                        raise UserError(_('The country is not matching the phone number'))
+                    elif 'valid' in response.keys() and not response.get('valid'):
+                        raise UserError(_('Phone Number is not correct'))
+                elif enable_verifynum and not verifynum_api_key:
+                    raise UserError(_('NumVerify is enabled but the Access Key is not configured!'))
+                else:
+                    pass
+
+    @api.constrains('country_id', 'mobile')
+    def _check_country_vs_mobile(self):
+        for rec in self:
+            if rec.country_id and rec.mobile:
+                # Get the country's code for the country that the user has entered
+                country_code = rec.country_id.code
+
+                # Check if the VerifyNum is enabled
+
+                params = rec.env['ir.config_parameter'].sudo()
+                enable_verifynum=params.get_param('enable_verifynum')
+                verifynum_api_key=params.get_param('verifynum_api_key', default='')
+
+                if enable_verifynum and verifynum_api_key:
+                    url = 'http://apilayer.net/api/validate?access_key=%s&number=%s&format=1' % (verifynum_api_key, rec.mobile)
+                    response = requests.get(url).json()
+                    # Raise the errors if something was not valid.
+                    if 'valid' in response.keys() and response.get('valid') and not response.get('country_code') == country_code:
+                        raise UserError(_('The country is not matching the mobile number'))
+                    elif 'valid' in response.keys() and not response.get('valid'):
+                        raise UserError(_('Mobile Number is not correct'))
+                elif enable_verifynum and not verifynum_api_key:
+                    raise UserError(_('NumVerify is enabled but the Access Key is not configured!'))
+                else:
+                    pass
